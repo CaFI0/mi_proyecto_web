fdc42120f6a29dd448920c5c0ec6312772a574d7
Cafio <cafio@protonmail.com>
2021-04-07 19:06:21 -0500

    voy a eliminar la carpeta de otros

---
+++ js/codigo.js
@@ -0,0 +1,16 @@
+//personas
+var amigos = ["perales", "jesus", "emilio", "angel"];
+//document.write("tienes " + amigos.length + "amigos");
+amigos[amigos.length] = "focus";
+amigos[amigos.length] = "proton";
+
+amigos.push("luis", "oscar", "foton")
+
+amigos.pop();
+
+document.write(0)
+document.write(1)
+document.write(2)
+document.write(3)
+document.write(4)
+
